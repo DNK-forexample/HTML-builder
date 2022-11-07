73ab9f8ed066c3f18676d43674c0d37229be43ae
Dasha Konchyts <forexamplednk@gmail.com>
2022-11-07 22:23:05 +0300

    feat: add js code to write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,2 +1,22 @@
 
+ const fs = require('fs');
+ const path = require('path');
+ const { stdin, stdout } = require('process');
  
+ const writable = fs.createWriteStream(
+   path.join(__dirname, 'text.txt')
+   );
+   
+   stdout.write('Please, enter your text below\n');
+   //stdin.on('data', data => writable.write(data));
+   stdin.on('data', data => {
+     if (data.toString().trim() === 'exit') process.exit();
+     writable.write(data);
+   });
+   
+   process.on('exit', () => stdout.write('You are out of input mode'));
+   process.on('SIGINT', () => {
+    process.stdout.write('You are out of input mode')
+    process.exit();
+  })
+   
